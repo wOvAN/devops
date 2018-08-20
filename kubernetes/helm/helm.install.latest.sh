0471209dbc1584ac04c28c121cbc05bb97540df7
wOvAN <wxsoft.ru@gmail.com>
2018-08-20 16:37:45 +0500

    +helm

---
+++ kubernetes/helm/helm.install.latest.sh
@@ -0,0 +1,3 @@
+curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh
+chmod 700 get_helm.sh -v latest
+./get_helm.sh
---
+++ kubernetes/helm/helm.install.ver.sh
@@ -0,0 +1,5 @@
+ver v2.10.0
+
+curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh
+chmod 700 get_helm.sh -v ${ver}
+./get_helm.sh
