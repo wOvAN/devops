a4fbda3d9ec6fc0396de65de9a7e35e085db8960
wOvAN <wxsoft.ru@gmail.com>
2018-08-20 12:28:11 +0300

    +ssl

---
+++ ssl/dump.certs.sh
@@ -0,0 +1,11 @@
+CERT_DIR=./certs_dump
+
+SERVER=example.com
+PORT=443
+CERTIFICATE=$CERT_DIR/${SERVER}.crt
+
+# Create the certificates hierarchy expected by gitlab
+sudo mkdir -p $(dirname "$CERTIFICATE")
+
+# Get the certificate in PEM format and store it
+openssl s_client -connect ${SERVER}:${PORT} -servername $SERVER  -showcerts </dev/null 2>/dev/null | sed -e '/-----BEGIN/,/-----END/!d' | sudo tee "$CERTIFICATE" >/dev/null
