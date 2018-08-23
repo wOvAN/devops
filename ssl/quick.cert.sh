912faf701cd78b7ccb5e4a091b2e8a4035a80547
wOvAN <wxsoft.ru@gmail.com>
2018-08-23 11:45:52 +0500

    ssl quick cert upd

--- ssl/quick.cert.sh
+++ ssl/quick.cert.sh
@@ -12,6 +12,6 @@ key_len=4096
 #################################################################################
 dns_name="site.example.com"
 
-openssl req -x509 -newkey rsa:${key_len} ${sha} -keyout ${dns_name}.key -out ${dns_name}.crt -days ${days} \
+openssl req -x509 -newkey rsa:${key_len} -${sha} -keyout ${dns_name}.key -out ${dns_name}.crt -days ${days} \
     -nodes \
     -subj "/C=${country}/ST=${state}/emailAddress=${email}/CN=${dns_name}/L=$location/O=${org}/OU=${org_unit}"
