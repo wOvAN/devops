50bf572424cbcc14820995476fa2455d5f22ec07
wOvAN <wxsoft.ru@gmail.com>
2018-08-23 11:42:07 +0500

    ssl quick cert upd

--- ssl/quick.cert.sh
+++ ssl/quick.cert.sh
@@ -6,8 +6,12 @@ email="email@example.com"
 org="Example org"
 org_unit="Example department"
 ### CA Params ###################################################################
+days=1825
+sha=sha512
+key_len=4096
+#################################################################################
 dns_name="site.example.com"
 
-openssl req -x509 -newkey rsa:4096 -keyout ${dns_name}.key -out ${dns_name}.crt -days 1095 \
+openssl req -x509 -newkey rsa:${key_len} ${sha} -keyout ${dns_name}.key -out ${dns_name}.crt -days ${days} \
     -nodes \
     -subj "/C=${country}/ST=${state}/emailAddress=${email}/CN=${dns_name}/L=$location/O=${org}/OU=${org_unit}"
