8923dfb439fe411eff1a01142c83933a8975570c
wOvAN <wxsoft.ru@gmail.com>
2018-10-23 18:26:23 +0500

    fx gitlab.registry.cleanup.cron.sh

--- gitlab/scripts/gitlab.registry.cleanup.cron.sh
+++ gitlab/scripts/gitlab.registry.cleanup.cron.sh
@@ -2,7 +2,7 @@
 tmp_file="gitlab.registry.cleanup.cron"
 crontab -l > $tmp_file
 
-echo "0 11 * * 1 docker exec -it gitlab gitlab-ctl registry-garbage-collect >> /var/log/registry-garbage-collect.log" >> $tmp_file
+echo "19 * * * 1 /usr/bin/docker exec -t gitlab gitlab-ctl registry-garbage-collect >> /var/log/registry-garbage-collect.log" >> $tmp_file
 
 # install new cron file
 crontab $tmp_file
