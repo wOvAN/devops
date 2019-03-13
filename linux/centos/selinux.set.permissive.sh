eafc2ddcaf406c37fcb7daa1c31e9c72ae29b06c
mf_app <mf_app@megafon.ru>
2019-03-13 19:43:24 +0500

    selinux script

---
+++ linux/centos/selinux.set.permissive.sh
@@ -0,0 +1,7 @@
+echo "--- SESTATUS OLD ------------------------------------------------"
+sestatus
+echo "--- SESTATUS NEW ------------------------------------------------"
+sudo setenforce 0
+sudo sed -i 's/SELINUX=.*/SELINUX=permissive/g' /etc/selinux/config"
+sestatus
+echo "-----------------------------------------------------------------"
