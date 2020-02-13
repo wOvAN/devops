45774957af9ad5cd3d0be6b69c3b8a651af088d1
wOvAN <wovan.dev@protonmail.com>
2020-02-13 09:48:57 +0500

    upd: quick_cert

--- ssl/quick.cert.sh
+++ ssl/quick.cert.sh
@@ -10,8 +10,14 @@ days=1825
 sha=sha512
 key_len=4096
 #################################################################################
-dns_name="site.example.com"
 
-openssl req -x509 -newkey rsa:${key_len} -${sha} -keyout ${dns_name}.key -out ${dns_name}.crt -days ${days} \
-    -nodes \
-    -subj "/C=${country}/ST=${state}/emailAddress=${email}/CN=${dns_name}/L=$location/O=${org}/OU=${org_unit}"
+dns_names=(
+    "dns.1.1.1.1.xip.io"
+    "dns.8.8.8.8.xip.io"
+)
+
+for dns_name in ${dns_names[*]}; do
+    openssl req -x509 -newkey rsa:${key_len} -${sha} -keyout ${dns_name}.key -out ${dns_name}.crt -days ${days} \
+	-nodes \
+	-subj "/C=${country}/ST=${state}/emailAddress=${email}/CN=${dns_name}/L=$location/O=${org}/OU=${org_unit}"
+done
