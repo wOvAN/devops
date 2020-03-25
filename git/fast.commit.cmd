caf6748873f68259071a294f32d9a40592ea8162
wOvAN <wovan.dev@protonmail.com>
2020-03-25 20:29:04 +0500

    update 2020-03-25 20:29:04

---
+++ git/fast.commit.cmd
@@ -0,0 +1,13 @@
+@echo off
+
+IF "%time:~0,1%" LSS "1" (
+   SET DATETIME=%date:~6,4%-%date:~3,2%-%date:~0,2% 0%time:~1,1%:%time:~3,2%:%time:~6,2%
+) ELSE (
+   SET DATETIME=%date:~6,4%-%date:~3,2%-%date:~0,2% %time:~0,2%:%time:~3,2%:%time:~6,2%
+)
+
+:: ECHO %DATETIME%
+
+git add .
+git commit -m "update %DATETIME%"
+git push
---
+++ git/git.trust.repo.cert.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CERT_DIR=~/.git-certs
+SERVER="gitlab.wxsoft.ru"
+PORT=443
+CERTIFICATE="${CERT_DIR}/certs.crt"
+
+# Create the certificates hierarchy expected by gitlab
+mkdir -p ${CERT_DIR}
+
+# Get the certificate in PEM format and store it
+openssl s_client -connect ${SERVER}:${PORT} \
+    -servername ${SERVER} \
+    -showcerts </dev/null 2>/dev/null | sed -e '/-----BEGIN/,/-----END/!d' | sudo tee "${CERTIFICATE}" >/dev/null
+
+git config --global http.sslCAInfo ${CERTIFICATE}
+
+
